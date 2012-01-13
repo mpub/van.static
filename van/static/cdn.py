7b0c39251760b83098faf462d6baf02cd559eb44
Can Burak Cilingir <can@canb.net>
2012-01-13 19:08:42 +0800

    pep8 compability fixes

--- van/static/cdn.py
+++ van/static/cdn.py
@@ -6,9 +6,12 @@ import optparse
 import tempfile
 import subprocess
 
-from pkg_resources import get_distribution, resource_listdir, resource_isdir, resource_filename
+from pkg_resources import (get_distribution, resource_listdir, resource_isdir,
+                           resource_filename)
 
-def extract_cmd(resources=None, target=None, yui_compressor=False, args=sys.argv):
+
+def extract_cmd(resources=None, target=None, yui_compressor=False,
+                args=sys.argv):
     """Export from the command line"""
     parser = optparse.OptionParser(usage="usage: %prog [options]")
     res_help = "Resource to dump (may be repeated)."
@@ -18,12 +21,18 @@ def extract_cmd(resources=None, target=None, yui_compressor=False, args=sys.argv
                       dest="resources",
                       action='append',
                       help=res_help)
-    parser.add_option("--yui-compressor", dest="yui_compressor", action="store_true",
-                      help="Compress the files with the yui-compressor (must be on the path)")
-    parser.add_option("--no-yui-compressor", dest="yui_compressor", action="store_false",
+    parser.add_option("--yui-compressor", dest="yui_compressor",
+                      action="store_true",
+                      help=("Compress the files with the yui-compressor "
+                            "(must be on the path)"))
+    parser.add_option("--no-yui-compressor", dest="yui_compressor",
+                      action="store_false",
                       help="Do not compress the files with yui-compressor")
     parser.add_option("--target", dest="target",
-                      help="Where to put the resources (can be the name of a local directory, or a url on S3 (eg: s3://bucket_name/path) you will need s3cmd available to push the files")
+                      help=("Where to put the resources (can be the name of a "
+                            "local directory, or a url on S3 "
+                            "(eg: s3://bucket_name/path) you will need s3cmd "
+                            "available to push the files"))
     parser.add_option("--aws-access-key", dest="aws_access_key",
                       help="AWS access key")
     parser.add_option("--aws-secret-key", dest="aws_secret_key",
@@ -52,6 +61,7 @@ def extract_cmd(resources=None, target=None, yui_compressor=False, args=sys.argv
     assert len(args) == 1, args
     extract(options.resources, options.target, options.yui_compressor, **kw)
 
+
 def extract(resources, target, yui_compressor=True, **kw):
     """Export the resources"""
     r_files = _walk_resources(resources)
@@ -66,12 +76,14 @@ def extract(resources, target, yui_compressor=True, **kw):
         if comp is not None:
             comp.dispose()
 
+
 def _get_putter(target, **kw):
     schema = target.split(':')[0]
     putter = {
             'file': _PutLocal,
             's3': _PutS3}[schema]
     return putter(target, **kw)
+
 
 def config_static(config, static_resources, static_cdn=None):
     """Configure a Pyramid application with a list of static resources.
@@ -82,7 +94,9 @@ def config_static(config, static_resources, static_cdn=None):
     If static_cdn is a URL, resources will be loaded from there under this
     schema:
 
-        http://cdn.example.com/some/path/${package_name}/${package_version}/path/within/pacakage
+        http://cdn.example.com/path/${package_name}/${package_version}/path
+
+    Note that `path` is the path to the resource within the package.
     """
     if static_cdn is None:
         for name, path in static_resources:
@@ -92,8 +106,10 @@ def config_static(config, static_resources, static_cdn=None):
         for name, path in static_resources:
             pname, filepath = path.split(':', 1)
             dist = get_distribution(pname)
-            name = '/'.join([static_cdn, dist.project_name, dist.version, filepath])
+            name = '/'.join([static_cdn, dist.project_name, dist.version,
+                             filepath])
             config.add_static_view(name=name, path=path)
+
 
 def _walk_resource_directory(pname, resource_directory):
     """Walk a resource directory and yield all files.
@@ -106,12 +122,15 @@ def _walk_resource_directory(pname, resource_directory):
             continue
         r_path = '/'.join([resource_directory, member])
         if resource_isdir(pname, r_path):
-            logging.info("_walk_resource_directory: Recursing into directory %s:%s", pname, r_path)
+            logging.info("_walk_resource_directory: Recursing into directory "
+                         "%s:%s", pname, r_path)
             for r in _walk_resource_directory(pname, r_path):
                 yield r
         else:
-            logging.info("_walk_resource_directory: Found resource %s:%s", pname, r_path)
+            logging.info("_walk_resource_directory: Found resource "
+                         "%s:%s", pname, r_path)
             yield r_path, 'file'
+
 
 def _walk_resources(resources):
     for res in resources:
@@ -122,6 +141,7 @@ def _walk_resources(resources):
         for r, type in resources:
             fs_r = resource_filename(pname, r)
             yield r, fs_r, pname, dist, type
+
 
 class _PutLocal:
 
@@ -143,10 +163,13 @@ class _PutLocal:
     def put(self, files):
         proj_dirs = set([])
         for rpath, fs_rpath, pname, dist, type in files:
-            fs_path = rpath.replace('/', os.sep) # enough for windows?
-            target = os.path.join(self._target_dir, dist.project_name, dist.version, fs_path)
+            fs_path = rpath.replace('/', os.sep)  # enough for windows?
+            target = os.path.join(self._target_dir, dist.project_name,
+                                  dist.version, fs_path)
             if pname not in proj_dirs:
-                self._if_not_exist(os.makedirs, os.path.join(self._target_dir, dist.project_name, dist.version))
+                self._if_not_exist(os.makedirs, os.path.join(self._target_dir,
+                                                             dist.project_name,
+                                                             dist.version))
                 proj_dirs.add(pname)
             if type == 'file':
                 self._copy(fs_rpath, target)
@@ -192,8 +215,10 @@ class _PutS3:
         for rpath, fs_rpath, pname, dist, type in files:
             if type == 'dir':
                 continue
-            logging.debug("putting to S3: %s", (rpath, fs_rpath, pname, dist, type))
-            target = '/'.join([self._path, dist.project_name, dist.version, rpath])
+            logging.debug("putting to S3: %s",
+                          (rpath, fs_rpath, pname, dist, type))
+            target = '/'.join([self._path, dist.project_name, dist.version,
+                               rpath])
             key = Key(bucket)
             key.key = target
             key.set_contents_from_filename(
@@ -201,6 +226,7 @@ class _PutS3:
                     reduced_redundancy=True,
                     headers={'Cache-Control': 'max-age=32140800'},
                     policy='public-read')
+
 
 class _YUICompressor:
 
@@ -210,7 +236,8 @@ class _YUICompressor:
 
     def dispose(self):
         if self._tmpdir is not None:
-            logging.debug("_YITCompressior: removing temp workspace: %s", self._tmpdir)
+            logging.debug("_YITCompressior: removing temp workspace: %s",
+                          self._tmpdir)
             shutil.rmtree(self._tmpdir)
             self._tmpdir = None
 
@@ -227,9 +254,11 @@ class _YUICompressor:
                 yield rpath, fs_rpath, pname, dist, f_type
                 continue
             self._counter += 1
-            target = os.path.join(self._tmpdir, str(self._counter) + '-' + os.path.basename(fs_rpath))
+            target = os.path.join(self._tmpdir, str(self._counter) + '-' +
+                                                os.path.basename(fs_rpath))
             args = ['yui-compressor', '--type', type, '-o', target, fs_rpath]
-            logging.debug('Compressing with YUI Compressor %s file, from %s to %s', type, fs_rpath, target)
+            logging.debug('Compressing with YUI Compressor %s file, '
+                          'from %s to %s', type, fs_rpath, target)
             subprocess.check_call(args)
             yield rpath, target, pname, dist, f_type
 
