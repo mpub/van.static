9bb5aab76aa955a8b3b7d125ed5511bf5085d4e3
Brian Sutherland <brian@vanguardistas.net>
2016-11-04 11:29:42 +0100

    svb

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = f.read()
 f.close()
 
 setup(name="van.static",
-      version='1.5',
+      version='1.6',
       license='BSD-derived',
       long_description=long_description,
       url='http://pypi.python.org/pypi/van.static',
